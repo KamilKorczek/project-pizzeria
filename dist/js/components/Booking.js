11466d82a5cc0dd4cff9f554a731b01cc76f4c1d
KamilKorczek <korczek@gmail.com>
2022-09-30 19:54:34 +0200

    The content on the second tab of the pizzeria_s website has been displayed. Booking class and a file with the same name have been added

---
+++ dist/js/components/Booking.js
@@ -0,0 +1,35 @@
+import {templates, select} from '../settings.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element) {
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidget();
+  }
+
+  render(element) {
+    const thisBooking = this;
+    const generatedHTML = templates.bookingWidget();
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element,
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidget() {
+    const thisBooking = this;
+    
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    
+    thisBooking.dom.wrapper.addEventListener('updated', function() {
+
+    });
+  }
+}
+
+export default Booking;
